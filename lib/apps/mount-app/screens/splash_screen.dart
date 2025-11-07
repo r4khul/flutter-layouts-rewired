356b80759f4af25044b377457c0051d3301a4f8c
BlackEmpir7199 <blackempire9177@gmail.com>
2025-11-07 16:50:04 +0530

    added a custom slapsh screen

---
+++ lib/apps/mount-app/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:layouts_practice/apps/mount-app/screens/home_screen.dart';
+
+final Color mainColor = Color(0xFFFF5656);
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>  HomeScreen()));
+    });
+
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.terrain, size: 80, color: Colors.white),
+          Container(
+            margin: EdgeInsets.only(left: 190, right: 190),
+            height: 50,
+            width: 40,
+            child: CircularProgressIndicator(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
