8d5f629e29179b2e0baf6caaa6e38f2724d514e1
BlackEmpir7199 <blackempire9177@gmail.com>
2025-11-07 19:00:17 +0530

    model: added category model and mock data

---
+++ lib/apps/mount-app/constants/mock_category_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:layouts_practice/apps/mount-app/models/category.dart';
+
+final List<CategoryModel> categories = [
+  CategoryModel(category: 'Mountain', icon: Icons.terrain),
+  CategoryModel(category: 'Forest', icon: Icons.park),
+  CategoryModel(category: 'Beach', icon: Icons.beach_access),
+  CategoryModel(category: 'Hiking', icon: Icons.directions_walk),
+];
